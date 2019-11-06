c63424b4cb10df5ab048d3f120926186963e9f04
pauso92 <53586556+pauso92@users.noreply.github.com>
2019-11-06 17:58:33 -0600

    ESLint

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+
+//Componente Principal HelloWorld
+const HelloWorld = () =>(
+    <h1>
+        Hola mundo
+    </h1>
+);
+
+export default HelloWorld;
+
